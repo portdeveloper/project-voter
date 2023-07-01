b7bfd6c5107c560a3a81a1b0bf11705fe7d18dde
port <schoolreef2@protonmail.com>
2023-07-02 01:04:08 +0300

    Implement deploy-voter page

--- packages/nextjs/pages/deploy-voter.tsx
+++ packages/nextjs/pages/deploy-voter.tsx
@@ -1,9 +1,58 @@
+import { useState } from "react";
+import { ethers } from "ethers";
 import type { NextPage } from "next";
+import { useContractWrite, usePrepareContractWrite } from "wagmi";
+import { useDeployedContractInfo } from "~~/hooks/scaffold-eth";
 
 const DeployVoter: NextPage = () => {
+  const [owner, setOwner] = useState("");
+  const [votingPeriodInDays, setVotingPeriodInDays] = useState(0);
+  const [hackathonName, setHackathonName] = useState("");
+
+  let PVFAddress: any;
+  let PVFAbi: any;
+
+  const { data: deployedContractData } = useDeployedContractInfo("ProjectVoterFactory");
+  if (deployedContractData) {
+    ({ address: PVFAddress, abi: PVFAbi } = deployedContractData);
+  }
+
+  const { config } = usePrepareContractWrite({
+    address: PVFAddress,
+    abi: PVFAbi,
+    functionName: "createProjectVoter",
+    args: [owner, ethers.BigNumber.from(votingPeriodInDays), hackathonName],
+  });
+
+  const { write } = useContractWrite(config);
+
   return (
-    <div>
-      <p>DeployVoter DeployVoter DeployVoter</p>
+    <div className="flex justify-center mx-auto">
+      <div className="mx-auto mt-10 w-full max-w-6xl space-y-2">
+        <div className="rounded-lg bg-white p-4 shadow">
+          <input
+            type="text"
+            placeholder="Owner address"
+            onChange={e => setOwner(e.target.value)}
+            className="input w-full mb-4 shadow-md"
+          />
+          <input
+            type="number"
+            placeholder="Voting period in days"
+            onChange={e => setVotingPeriodInDays(parseInt(e.target.value))}
+            className="input w-full mb-4 shadow-md"
+          />
+          <input
+            type="text"
+            placeholder="Hackathon name"
+            onChange={e => setHackathonName(e.target.value)}
+            className="input w-full mb-4 shadow-md"
+          />
+          <button onClick={() => write?.()} className="btn btn-primary w-full">
+            Deploy
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
